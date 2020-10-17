c8ee60c8a6d2543ba91ad129de531053b0ca6056
MichaelOmondi <omondimike11@gmail.com>
2020-10-17 12:42:09 +0300

    started a django project called Instaagram

---
+++ Instagram/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import -My-profile,Posts,Following,Comments
+
+admin.site.register(My-profile)
+admin.site.register(Posts)
+admin.site.register(Following)
+admin.site.register(Comments)
---
+++ Instagram/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#Profile section
+class Profile(models.Model):
+    
+
