804098b3ab1c645ae89f74640f1af1ec819866c6
MichaelOmondi <omondimike11@gmail.com>
2020-10-17 13:13:57 +0300

    created a class posts

--- Instagram/models.py
+++ Instagram/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
 
     def __str__(self):
        return f'{self.user.username}'  
-       
+
 # User Following
 class Following(models.Model):
     username = models.CharField(blank=True,max_length=245)
@@ -18,6 +18,17 @@ class Following(models.Model):
 
     def __str__(self):
         return f'{self.username}'
+
+# User Posts
+class Post(models,Model):
+    pic = models.ImageField(upload_to = 'posts/')
+    caption = models.CharField(blank=True,max_length=245)
+    Profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
+
+    def __str__ (self):
+        return f'{self.Profile.user.username}'
+               
 
 
 
