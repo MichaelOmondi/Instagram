39ca8daa1f1ef8f8ddf1b04c5af3be6d8dd3d5f6
MichaelOmondi <omondimike11@gmail.com>
2020-10-17 13:03:13 +0300

    creat class Following

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Profile section
+# User Profile 
 class Profile(models.Model):
 
     User = models.OneToOneField (User, on_delete=models.CASCADE)  
     Profile_pic = models.ImageField(blank=True,uplad_to = 'image/')
-    bio = models.CharField(max_length=245)  
+    bio = models.CharField(max_length=245)
+
+    def __str__(self):
+       return f'{self.user.username}'  
+       
+# User Following
+class Following(models.Model):
+    username = models.CharField(blank=True,max_length=245)
+    followed = models.CharField(blank=True,max_length=245)
+
+    def __str__(self):
+        return f'{self.username}'
+
+
 
