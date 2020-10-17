42497a24926ddfc6d9f234292f59f0969d803ccf
MichaelOmondi <omondimike11@gmail.com>
2020-10-17 18:10:47 +0300

    imported the profile, fllowing and posts

--- IG/views.py
+++ IG/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile,Posts,Following,Comments
+from .forms import DetailsForm,PostForm
+from django.db.models import F
 
 # Create your views here.
+
+def welcome(request):
+    return render(request,'welcome.html')
+
+
+@login_required(login_url='/acounts/login/')    
