0c12bb975dd63c1ed1b43fc743ad1dd252c8f2ac
MichaelOmondi <omondimike11@gmail.com>
2020-10-17 12:51:32 +0300

    created a class profile

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import User
 
 #Profile section
 class Profile(models.Model):
-    
+
+    User = models.OneToOneField (User, on_delete=models.CASCADE)  
+    Profile_pic = models.ImageField(blank=True,uplad_to = 'image/')
+    bio = models.CharField(max_length=245)  
 
